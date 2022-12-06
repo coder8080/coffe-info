0e0ec7181d59901d32fee565803e5673f9ed3c84
Roman Kolerov <coder8080@mail.ru>
2022-12-06 09:01:59 +0300

    Подготовка к релизу

--- main.py
+++ main.py
@@ -1,10 +1,120 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QTableWidgetItem, QPushButton
-from PyQt5 import uic
 from sys import exit, argv
 import sqlite3
 
 
-class addEditWindow(QMainWindow):
+from PyQt5 import QtCore, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1167, 551)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.table = QtWidgets.QTableWidget(self.centralwidget)
+        self.table.setGeometry(QtCore.QRect(10, 10, 1151, 481))
+        self.table.setObjectName("table")
+        self.table.setColumnCount(0)
+        self.table.setRowCount(0)
+        self.add_button = QtWidgets.QPushButton(self.centralwidget)
+        self.add_button.setGeometry(QtCore.QRect(10, 500, 241, 42))
+        self.add_button.setObjectName("add_button")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Капучино"))
+        self.add_button.setText(_translate("MainWindow", "Добавить запись"))
+
+
+class Ui_addEditWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(392, 370)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(10, 10, 383, 291))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.name_edit = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.name_edit.setObjectName("name_edit")
+        self.formLayout.setWidget(
+            0, QtWidgets.QFormLayout.ItemRole.LabelRole, self.name_edit)
+        self.name_label = QtWidgets.QLabel(self.formLayoutWidget)
+        self.name_label.setObjectName("name_label")
+        self.formLayout.setWidget(
+            0, QtWidgets.QFormLayout.ItemRole.FieldRole, self.name_label)
+        self.degree_box = QtWidgets.QComboBox(self.formLayoutWidget)
+        self.degree_box.setObjectName("degree_box")
+        self.formLayout.setWidget(
+            1, QtWidgets.QFormLayout.ItemRole.LabelRole, self.degree_box)
+        self.degree_label = QtWidgets.QLabel(self.formLayoutWidget)
+        self.degree_label.setObjectName("degree_label")
+        self.formLayout.setWidget(
+            1, QtWidgets.QFormLayout.ItemRole.FieldRole, self.degree_label)
+        self.type_box = QtWidgets.QComboBox(self.formLayoutWidget)
+        self.type_box.setObjectName("type_box")
+        self.formLayout.setWidget(
+            2, QtWidgets.QFormLayout.ItemRole.LabelRole, self.type_box)
+        self.type_label = QtWidgets.QLabel(self.formLayoutWidget)
+        self.type_label.setObjectName("type_label")
+        self.formLayout.setWidget(
+            2, QtWidgets.QFormLayout.ItemRole.FieldRole, self.type_label)
+        self.description_edit = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.description_edit.setObjectName("description_edit")
+        self.formLayout.setWidget(
+            3, QtWidgets.QFormLayout.ItemRole.LabelRole, self.description_edit)
+        self.description_label = QtWidgets.QLabel(self.formLayoutWidget)
+        self.description_label.setObjectName("description_label")
+        self.formLayout.setWidget(
+            3, QtWidgets.QFormLayout.ItemRole.FieldRole, self.description_label)
+        self.price_box = QtWidgets.QSpinBox(self.formLayoutWidget)
+        self.price_box.setMaximum(100000)
+        self.price_box.setObjectName("price_box")
+        self.formLayout.setWidget(
+            4, QtWidgets.QFormLayout.ItemRole.LabelRole, self.price_box)
+        self.price_label = QtWidgets.QLabel(self.formLayoutWidget)
+        self.price_label.setObjectName("price_label")
+        self.formLayout.setWidget(
+            4, QtWidgets.QFormLayout.ItemRole.FieldRole, self.price_label)
+        self.volume_box = QtWidgets.QSpinBox(self.formLayoutWidget)
+        self.volume_box.setMaximum(100000)
+        self.volume_box.setObjectName("volume_box")
+        self.formLayout.setWidget(
+            5, QtWidgets.QFormLayout.ItemRole.LabelRole, self.volume_box)
+        self.volume_label = QtWidgets.QLabel(self.formLayoutWidget)
+        self.volume_label.setObjectName("volume_label")
+        self.formLayout.setWidget(
+            5, QtWidgets.QFormLayout.ItemRole.FieldRole, self.volume_label)
+        self.save_button = QtWidgets.QPushButton(self.centralwidget)
+        self.save_button.setGeometry(QtCore.QRect(260, 320, 124, 42))
+        self.save_button.setObjectName("save_button")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "not set"))
+        self.name_label.setText(_translate("MainWindow", "Название сорта"))
+        self.degree_label.setText(_translate("MainWindow", "Степень прожарки"))
+        self.type_label.setText(_translate("MainWindow", "Тип"))
+        self.description_label.setText(
+            _translate("MainWindow", "Описание вкуса"))
+        self.price_label.setText(_translate("MainWindow", "Цена упаковки"))
+        self.volume_label.setText(_translate("MainWindow", "Объём упаковки"))
+        self.save_button.setText(_translate("MainWindow", "Сохранить"))
+
+
+class addEditWindow(QMainWindow, Ui_addEditWindow):
     def __init__(self, type: str, con, cur, id=None, on_close=None):
         if type not in ['add', 'edit']:
             raise ValueError(
@@ -31,7 +141,7 @@ class addEditWindow(QMainWindow):
         self.type_box.addItems([el[1] for el in self.available_types])
 
     def initUi(self):
-        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setupUi(self)
         self.save_button.clicked.connect(self.submit)
         self.update_combo_boxes()
         if self.type == 'edit':
@@ -91,7 +201,7 @@ class addEditWindow(QMainWindow):
         self.close()
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.data = []
@@ -100,7 +210,7 @@ class MainWindow(QMainWindow):
         self.initUi()
 
     def initUi(self):
-        uic.loadUi('main.ui', self)
+        self.setupUi(self)
         self.add_button.clicked.connect(self.open_add_window)
         self.table.setColumnCount(8)
         self.table.setHorizontalHeaderLabels(('ID', 'Название сорта', 'Степень прожарки',
